8df99f465b99ccef8fe1a13233596c1c038d131c
Shaanjot Gill <gill.shaanjots@gmail.com>
2022-08-23 08:21:47 -0700

    int test updates
    Signed-off-by: Shaanjot Gill <gill.shaanjots@gmail.com>

--- int/tests/conftest.py
+++ int/tests/conftest.py
@@ -149,6 +149,6 @@ async def connection(
 
 @pytest.fixture(scope="session")
 async def redis():
-    redis = RedisCluster.from_url(url="redis://redis-host/0")
+    redis = RedisCluster.from_url(url="redis://default:test1234@172.28.0.103:6379")
     yield redis
     await redis.close()
